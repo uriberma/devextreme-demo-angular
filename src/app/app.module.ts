a59d7b801793217c5ef3a612d186c46d1502f012
Maycol Uribe Romero <maycol.uribe-romero@hotmail.com>
2020-07-22 20:02:12 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SideNavOuterToolbarModule, SideNavInnerToolbarModule, SingleCardModule } from './layouts';
+import { FooterModule, LoginFormModule } from './shared/components';
+import { AuthService, ScreenService, AppInfoService } from './shared/services';
+import { AppRoutingModule } from './app-routing.module';
+import { PivotGridComponent } from './pages/pivot-grid/pivot-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PivotGridComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SideNavOuterToolbarModule,
+    SideNavInnerToolbarModule,
+    SingleCardModule,
+    FooterModule,
+    LoginFormModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthService, ScreenService, AppInfoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/pivot-grid/pivot-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pivot-grid',
+  templateUrl: './pivot-grid.component.html',
+  styleUrls: ['./pivot-grid.component.scss']
+})
+export class PivotGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
